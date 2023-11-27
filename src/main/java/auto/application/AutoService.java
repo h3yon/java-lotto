2dd596e7c7295fd5f4fc2ba7b4560a0755777cc5
h3yon <khykhy1006@gmail.com>
2023-11-28 03:02:20 +0900

    refactor: auto-lotto 로직 분리 및 불필요한 주석 제거
    refactor: lotto to lottery

--- src/main/java/auto/application/AutoService.java
+++ src/main/java/auto/application/AutoService.java
@@ -1,17 +1,24 @@
 package auto.application;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class AutoService {
-    private final static int LOTTO_MIN_NUMBER = 1;
-    private final static int LOTTO_MAX_NUMBER = 45;
+    private final static int LOTTERY_MIN_NUMBER = 1;
+    private final static int LOTTERY_MAX_NUMBER = 45;
 
-    public static List<Integer> createLotteryNumbers() {
-        List<Integer> numbers = IntStream.range(LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER + 1)
+    public static List<List<Integer>> createLotteryNumbersList(int lotteryCount) {
+        return IntStream.range(0, lotteryCount)
+                        .mapToObj(i -> AutoService.createLotteryNumbers())
+                        .collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    private static List<Integer> createLotteryNumbers() {
+        List<Integer> numbers = IntStream.range(LOTTERY_MIN_NUMBER, LOTTERY_MAX_NUMBER + 1)
                                          .boxed()
                                          .collect(Collectors.toList());
         Collections.shuffle(numbers);
